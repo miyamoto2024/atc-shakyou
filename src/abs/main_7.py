720b11e512726cb426f21c48414dbb9aa541b1f0
miyamoto2024 <miyamotoak10113@gmail.com>
2024-10-09 02:27:50 +0900

    追加

--- src/abs/main_7.py
+++ src/abs/main_7.py
@@ -2,3 +2,34 @@
 # 以下の2点が分かるかどうか。（「マンハッタン距離」を参照）
 # 座標x,yから座標xi,yiに移動する際にかかる時間d = |xi - x| + |yi - y|が分かること。
 # 使える時間dtとdの差が偶数だったら到着可能、奇数だったら到着不可能と分かること。
+
+N = int(input())
+T = []
+X = []
+Y = []
+
+# start
+T.append(0)
+X.append(0)
+Y.append(0)
+
+for i in range(N):
+  l = list(map(int, input().split()))
+  T.append(l[0])
+  X.append(l[1])
+  Y.append(l[2])
+
+flag = False
+for i in range(N):
+  distance = abs(X[i + 1] - X[i]) + abs(Y[i + 1] - Y[i])
+  t = T[i + 1] - T[i]
+  if (distance == t or t > distance and (t - distance) % 2 == 0):
+    flag = True
+  else:
+    flag = False
+    break
+  
+if (flag == True):
+  print("Yes")
+else:
+  print("No")
